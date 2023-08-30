3de98e64ae3392ccc91aefaa6b6c7690affcb352
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-08-29 18:45:19 -0700

    Make TaskControl subclasses accessible

--- src/npc_sessions/trials/__init__.py
+++ src/npc_sessions/trials/__init__.py
@@ -1,3 +1,6 @@
 from npc_sessions.trials.property_dict import PropertyDict
+from npc_sessions.trials.TaskControl.OptoTagging import OptoTagging
+from npc_sessions.trials.TaskControl.RFMapping import AudRFMapping, VisRFMapping
+from npc_sessions.trials.TaskControl.DynamicRouting1 import DynamicRouting1
 
-__all__ = ["PropertyDict"]
+__all__ = ["PropertyDict", "OptoTagging", "AudRFMapping", "VisRFMapping", "DynamicRouting1"]
