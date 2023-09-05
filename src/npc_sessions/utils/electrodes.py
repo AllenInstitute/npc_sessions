8cdf1a25bd8d0a278e103016c44d67508909c748
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2023-09-05 09:59:17 -0700

    clean up session, get units and electrodes

--- src/npc_sessions/utils/electrodes.py
+++ src/npc_sessions/utils/electrodes.py
@@ -1,166 +1,119 @@
 from __future__ import annotations
 
-import pickle
 import warnings
 
 import npc_lims
 import npc_session
-import numpy as np
-import numpy.typing as npt
 import pandas as pd
-import SimpleITK as sitk
 import upath
+import functools
 
-NETWORK_ELECTRODE_PATH = upath.UPath(
-    "//allen/programs/mindscope/workgroups/np-behavior/tissuecyte"
+S3_ELECTRODE_PATH = upath.UPath(
+    "s3://aind-scratch-data/arjun.sridhar/tissuecyte_cloud_processed"
 )
 
-
-def get_acronym_map() -> dict[str, int]:
-    # TODO get from allen brain map
-    return pickle.loads(
-        upath.UPath(
-            "//allen/programs/mindscope/workgroups/np-behavior/tissuecyte/field_reference/acrnm_map.pkl"
-        ).read_bytes()
-    )
-
-
-def get_annotation_volume() -> npt.NDArray[np.int64]:
-    # TODO get from somewhere in the cloud
-    return sitk.GetArrayFromImage(
-        sitk.ReadImage(
-            upath.UPath(
-                "//allen/programs/mindscope/workgroups/np-behavior/tissuecyte/field_reference/ccf_ano.mhd"
-            )
-        )
-    )
-
-
-def get_structure_acronym(
-    acronym_map: dict[str, int],
-    annotation_volume: npt.NDArray[np.int64],
-    point: tuple[int, int, int],
-) -> str:
-    if point[1] < 0:
-        return "root"
-
-    structure_ids = tuple(acronym_map.values())
-    labels = tuple(acronym_map.keys())
-
-    structure_id = annotation_volume[point[0], point[1], point[2]]
-    if structure_id in structure_ids:
-        index = structure_ids.index(structure_id)
-        label = labels[index]
-    else:
-        label = "root"
-
-    return label
-
-
-def get_electrode_files_from_network(
+@functools.cache
+def get_electrode_files_from_s3(
     session: str | npc_session.SessionRecord,
-) -> tuple[upath.UPath, ...] | None:
+) -> tuple[upath.UPath, ...]:
+    """
+    >>> electrode_files = get_electrode_files_from_s3('626791_2022-08-16')
+    >>> assert len(electrode_files) > 0
+    """
     session = npc_session.SessionRecord(session)
     all_subject_sessions = npc_lims.get_subject_data_assets(session.subject)
     raw_subject_assets = sorted(
         asset["name"]
-        for asset in all_subject_sessions
-        if npc_lims.is_raw_data_asset(asset)
-    )
+            for asset in all_subject_sessions
+            if npc_lims.is_raw_data_asset(asset)
+        )
+    # TODO: fix getting right day
     day = tuple(
         raw_subject_assets.index(asset) + 1
         for asset in raw_subject_assets
         if f"{session.subject}_{session.date}" in asset
     )[0]
 
-    subject_electrode_network_path = NETWORK_ELECTRODE_PATH / str(session.subject.id)
+    subject_electrode_network_path = S3_ELECTRODE_PATH / str(session.subject.id)
 
     if not subject_electrode_network_path.exists():
-        warnings.warn(f"Session {session} does not have any annotations", stacklevel=2)
-        return None
+        raise FileNotFoundError(f'CCF annotations for {session} have not been uploaded to s3')
 
     electrode_files = tuple(
         subject_electrode_network_path.glob(
-            f"Probe_*{day}_channels_{str(session.subject.id)}_warped.csv"
+            f"Probe_*{day}_channels_{str(session.subject.id)}_warped_processed.csv"
         )
     )
-    if not electrode_files:
-        warnings.warn(f"Session {session.id} channels have not been aligned")
-        return None
 
     return electrode_files
 
+def get_horizontal_vertical_positions(probe_electrodes: pd.DataFrame) -> tuple[list[int], list[int]]:
+    vertical_position = 20
+    horizontal_position_even = [43, 59]
+    horizontal_position = horizontal_position_even[0]
+    horizontal_position_even_index = 0
+    horizontal_position_odd = [11, 27]
+    horizontal_position_odd_index = 0
 
-def get_electrodes_from_network(
+    vertical_positions: list[int] = []
+    horizontal_positions: list[int] = []
+
+    for index, row in probe_electrodes.iterrows():
+        if index != 0 and index % 2 == 0:
+            vertical_position += 20
+
+        if index == 0:
+            horizontal_position = horizontal_position_even[0]
+        elif index == 1:
+            horizontal_position = horizontal_position_odd[0]
+        elif index != 0 and index % 2 == 0:
+            if horizontal_position_even_index == 0:
+                horizontal_position_even_index = 1
+                horizontal_position = horizontal_position_even[
+                    horizontal_position_even_index
+                ]
+            else:
+                horizontal_position_even_index = 0
+                horizontal_position = horizontal_position_even[
+                    horizontal_position_even_index
+                ]
+        elif index != 1 and index % 1 == 0:
+            if horizontal_position_odd_index == 0:
+                horizontal_position_odd_index = 1
+                horizontal_position = horizontal_position_odd[
+                    horizontal_position_odd_index
+                ]
+            else:
+                horizontal_position_odd_index = 0
+                horizontal_position = horizontal_position_odd[
+                    horizontal_position_odd_index
+                ]
+
+        horizontal_positions.append(horizontal_position)
+        vertical_positions.append(vertical_position)
+    
+    return horizontal_positions, vertical_positions
+    
+
+@functools.cache
+def create_tissuecyte_electrodes_table(
     session: str | npc_session.SessionRecord,
-) -> pd.DataFrame | None:
-    electrode_files = get_electrode_files_from_network(session)
-
-    if not electrode_files:
-        return None
+) -> pd.DataFrame:
+    electrode_files = get_electrode_files_from_s3(session)
 
     session_electrodes = None
-    volume = get_annotation_volume()
-    map = get_acronym_map()
 
     for electrode_file in electrode_files:
-        string_file = str(electrode_file)
-        probe = string_file[string_file.index("_") + 1 : string_file.index("_") + 2]
+        string_file = str(electrode_file.stem)
+        probe = string_file[string_file.index("_") + 1 : string_file.index("_") + 2].upper()
 
         probe_electrodes = pd.read_csv(electrode_file)
-
-        vertical_position = 20
-        horizontal_position_even = [43, 59]
-        horizontal_position = horizontal_position_even[0]
-        horizontal_position_even_index = 0
-        horizontal_position_odd = [11, 27]
-        horizontal_position_odd_index = 0
-
-        vertical_positions: list[int] = []
-        horizontal_positions: list[int] = []
-
-        for index, row in probe_electrodes.iterrows():
-            if pd.isna(row.region):
-                label = get_structure_acronym(map, volume, (row.AP, row.DV, row.ML))
-
-                probe_electrodes.loc[index, "region"] = label
-
-            if index != 0 and index % 2 == 0:
-                vertical_position += 20
-
-            if index == 0:
-                horizontal_position = horizontal_position_even[0]
-            elif index == 1:
-                horizontal_position = horizontal_position_odd[0]
-            elif index != 0 and index % 2 == 0:
-                if horizontal_position_even_index == 0:
-                    horizontal_position_even_index = 1
-                    horizontal_position = horizontal_position_even[
-                        horizontal_position_even_index
-                    ]
-                else:
-                    horizontal_position_even_index = 0
-                    horizontal_position = horizontal_position_even[
-                        horizontal_position_even_index
-                    ]
-            elif index != 1 and index % 1 == 0:
-                if horizontal_position_odd_index == 0:
-                    horizontal_position_odd_index = 1
-                    horizontal_position = horizontal_position_odd[
-                        horizontal_position_odd_index
-                    ]
-                else:
-                    horizontal_position_odd_index = 0
-                    horizontal_position = horizontal_position_odd[
-                        horizontal_position_odd_index
-                    ]
-
-            horizontal_positions.append(horizontal_position)
-            vertical_positions.append(vertical_position)
 
         probe_electrodes["device_name"] = [
             f"Probe{probe}" for i in range(len(probe_electrodes))
         ]
+
+        horizontal_positions, vertical_positions = get_horizontal_vertical_positions(probe_electrodes)
         probe_electrodes["probe_vertical_position"] = vertical_positions
         probe_electrodes["probe_horizontal_position"] = horizontal_positions
 
@@ -168,6 +121,15 @@ def get_electrodes_from_network(
             session_electrodes = probe_electrodes
         else:
             session_electrodes = pd.concat([session_electrodes, probe_electrodes])
+
+    if session_electrodes is not None:
+        session_electrodes.rename(columns={'AP': 'anterior_posterior_ccf_coordinate', 'DV': 'dorsal_ventral_ccf_coordinate',
+                                           'ML': 'left_right_ccf_coordinate', 'region': 'structure_acronym', 
+                                           'region_stripped': 'structure_layer'}, inplace=True)
+        
+        session_electrodes['anterior_posterior_ccf_coordinate'] *= 25
+        session_electrodes['dorsal_ventral_ccf_coordinate'] *= 25
+        session_electrodes['left_right_ccf_coordinate'] *= 25
 
     return session_electrodes
 
--- src/npc_sessions/utils/units.py
+++ src/npc_sessions/utils/units.py
@@ -4,112 +4,54 @@ import io
 import warnings
 
 import npc_lims
+import upath
+import functools
+import npc_sessions
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
-import polars as pl
-import upath
+import io
+import os
 
 
-def run_capsules_for_units_kilosort_codeocean(session_id: str) -> None:
-    raw_data_asset = npc_lims.get_session_raw_data_asset(session_id)
-    if raw_data_asset is None:
-        raise ValueError(
-            f"Session {session_id} has no raw data asset."
-        )  # TODO move to function in npc_lims
-    sorted_data_asset = npc_lims.get_session_sorted_data_asset(session_id)
-    if sorted_data_asset is None:
-        raise ValueError(
-            f"Session {session_id} has no sorted data asset."
-        )  # TODO move to function in npc_lims
-
-    capsule_results_units = npc_lims.run_capsule_and_get_results(
-        "980c5218-abef-41d8-99ed-24798d42313b", (raw_data_asset, sorted_data_asset)
-    )
-    npc_lims.register_session_data_asset(session_id, capsule_results_units)
-
-    units_no_peak_channel_asset = npc_lims.get_session_units_data_asset(session_id)
-    if units_no_peak_channel_asset is None:
-        raise ValueError(
-            f"Session {session_id} has no peak_channel data asset."
-        )  # TODO move to function in npc_lims
-
-    capsule_result_units_peak_channels = npc_lims.run_capsule_and_get_results(
-        "d1a5c3a8-8fb2-4cb0-8e9e-96e6e1d03ff1",
-        (raw_data_asset, sorted_data_asset, units_no_peak_channel_asset),
-    )
-    npc_lims.register_session_data_asset(session_id, capsule_result_units_peak_channels)
-
-
-def get_units_spike_paths_from_kilosort_codeocean_output(
-    units_s3_path: tuple[upath.UPath, ...]
-) -> tuple[upath.UPath, ...]:
-    units_path = next(path for path in units_s3_path if "csv" in str(path))
-    spike_times_path = next(path for path in units_s3_path if "spike" in str(path))
-    mean_waveforms_path = next(path for path in units_s3_path if "mean" in str(path))
-    sd_waveforms_path = next(path for path in units_s3_path if "sd" in str(path))
-
-    return units_path, spike_times_path, mean_waveforms_path, sd_waveforms_path
-
-
-def update_permissions_for_data_asset(session_id):
-    units_data_asset = (
-        npc_lims.codeocean.get_session_units_with_peak_channels_data_asset(session_id)
-    )
-
-    if not units_data_asset:
-        warnings.warn(f"No units found for session {session_id}", stacklevel=2)
-    else:
-        npc_lims.codeocean_client.update_permissions(
-            units_data_asset["id"], everyone="viewer"
-        )
-
-
-def get_units_spike_paths(
-    session_id: str, method: str = "kilosort_codeocean"
-) -> tuple[upath.UPath, ...]:
-    # records = npc_lims.NWBSqliteDBHub().get_records(
-    #   npc_lims.Units, session_id=session_id
-    # )
-    records = ()
-    if not records:
+@functools.cache
+def get_units_electrodes(session: str, units_method='codeocean_kilosort', electrode_method='tissuecyte') -> pd.DataFrame:
+    if units_method == 'codeocean_kilosort':
+        units_path = npc_lims.get_units_codeoean_kilosort_path_from_s3(session)
+        units = pd.read_csv(units_path, storage_options={'key': os.getenv('AWS_ACCESS_KEY_ID'), 
+                                                         'secret': os.getenv('AWS_SECRET_ACCESS_KEY')})
+    if electrode_method == 'tissuecyte':    
         try:
-            npc_lims.get_units_codeoean_kilosort_path_from_s3(session_id)
-        except FileNotFoundError:
-            # TODO: DO SOMETHING TO GET UNITS - Right now, run spike nwb capsule, and register units as data asset
-            if method == "kilosort_codeocean":
-                run_capsules_for_units_kilosort_codeocean(session_id)
-                update_permissions_for_data_asset(session_id)
-                return get_units_spike_paths_from_kilosort_codeocean_output(
-                    npc_lims.get_units_spikes_codeocean_kilosort_top_level_files(
-                        session_id
-                    )
-                )  # TODO @Arjun check this temp fix
+            electrodes = npc_sessions.create_tissuecyte_electrodes_table(session)
+            units = units.merge(electrodes, left_on=['device_name', 'peak_channel'], right_on=['device_name', 'channel'])
+            units.drop(columns=['channel'], inplace=True)
+        except FileNotFoundError as e:
+            print(str(e) + '. Returning units without electrodes')
+    
+    units.drop(columns=['electrodes'], inplace=True)
+    return units
 
-    return records
-
-
-def get_spike_times(
-    units: pd.DataFrame, spike_times_path: upath.UPath
-) -> dict[str, npt.NDArray[np.float64]]:
+@functools.cache
+def get_unit_spike_times_dict(session: str, unit_ids: tuple[str, ...], method='codeocean_kilosort') -> dict[str, npt.NDArray[np.float64]]:
+    # change function call depending on method
     spike_times_dict = {}
-    units_names = units["unit_name"]
-    with io.BytesIO(spike_times_path.read_bytes()) as f:
-        spike_times = np.load(f, allow_pickle=True)
-
-    for i in range(len(units_names)):
-        spike_times_dict[units_names[i]] = spike_times[i]
-
+    if method == 'codeocean_kilosort':
+        spike_times_path = npc_lims.get_spike_times_codeocean_kilosort_path_from_s3(session)
+        with io.BytesIO(spike_times_path.read_bytes()) as f:
+            spike_times = np.load(f, allow_pickle=True)
+        
+        for i in range(len(unit_ids)):
+            spike_times_dict[unit_ids[i]] = spike_times[i]
+    
     return spike_times_dict
 
+if __name__ == "__main__":
+    import doctest
 
-def get_units_spike_times(
-    units_spike_paths: tuple[upath.UPath, ...]
-) -> tuple[pd.DataFrame, dict[str, npt.NDArray[np.float64]]]:
-    units_path = next(path for path in units_spike_paths if "csv" in str(path))
-    spike_times_path = next(path for path in units_spike_paths if "spike" in str(path))
+    import dotenv
 
-    units = pl.read_csv(units_path.read_bytes()).to_pandas()
-    spike_times = get_spike_times(units, spike_times_path)
+    dotenv.load_dotenv(dotenv.find_dotenv(usecwd=True))
+    doctest.testmod(
+        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE)
+    )
 
-    return units, spike_times
