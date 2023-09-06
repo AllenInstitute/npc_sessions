d89c94eb0e90f1edf27ba2dc25c197b8f3232afb
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-05 20:27:09 -0700

    Add `get_trials` utility

--- src/npc_sessions/trials/__init__.py
+++ src/npc_sessions/trials/__init__.py
@@ -1,7 +1,37 @@
+from typing import Optional
 from npc_sessions.trials.property_dict import PropertyDict
 from npc_sessions.trials.TaskControl.DynamicRouting1 import DynamicRouting1
 from npc_sessions.trials.TaskControl.OptoTagging import OptoTagging
 from npc_sessions.trials.TaskControl.RFMapping import AudRFMapping, VisRFMapping
+
+import npc_sessions.utils as utils
+
+def get_trials(*stim_path: utils.PathLike, sync_path_or_data: Optional[utils.SyncPathOrDataset], **kwargs) -> tuple[PropertyDict, ...]:
+    """Get trials for a given stimulus file.
+
+    Parameters
+    ----------
+    stim_path : utils.PathLike
+        Path to stimulus file.
+
+    Returns
+    -------
+    PropertyDict
+        Trials for the given stimulus file.
+    """
+    trials = []
+    for stim in stim_path:
+        if "DynamicRouting1" in stim:
+            trials.append(DynamicRouting1(stim, sync_path_or_data, **kwargs))
+        elif "OptoTagging" in stim:
+            trials.append(OptoTagging(stim, sync_path_or_data, **kwargs))
+        elif "RFMapping" in stim:
+            trials.append(AudRFMapping(stim, sync_path_or_data, **kwargs))
+            trials.append(VisRFMapping(stim, sync_path_or_data, **kwargs))
+        else:
+            raise NotImplementedError(f"Stimulus file {stim} not supported.")
+    return tuple(trials)
+
 
 __all__ = [
     "PropertyDict",
