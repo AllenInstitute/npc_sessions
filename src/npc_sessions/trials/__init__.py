b61eec78619583d5cd2c21d45e659ad78a9a607e
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-06 17:13:59 -0700

    Fix imports

--- src/npc_sessions/trials/__init__.py
+++ src/npc_sessions/trials/__init__.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from npc_sessions.trials.TaskControl import TaskControl
 from npc_sessions.trials.property_dict import PropertyDict
 from npc_sessions.trials.TaskControl.DynamicRouting1 import DynamicRouting1
 from npc_sessions.trials.TaskControl.OptoTagging import OptoTagging
@@ -21,22 +22,25 @@ def get_trials(*stim_path: utils.PathLike, sync_path_or_data: Optional[utils.Syn
     """
     trials = []
     for stim in stim_path:
-        if "DynamicRouting1" in stim:
+        stim_name = utils.from_pathlike(stim).name
+        if "DynamicRouting1" in stim_name:
             trials.append(DynamicRouting1(stim, sync_path_or_data, **kwargs))
-        elif "OptoTagging" in stim:
+        elif "OptoTagging" in stim_name:
             trials.append(OptoTagging(stim, sync_path_or_data, **kwargs))
-        elif "RFMapping" in stim:
+        elif "RFMapping" in stim_name:
             trials.append(AudRFMapping(stim, sync_path_or_data, **kwargs))
             trials.append(VisRFMapping(stim, sync_path_or_data, **kwargs))
         else:
-            raise NotImplementedError(f"Stimulus file {stim} not supported.")
+            raise NotImplementedError(f"Stimulus file {stim_name} not supported.")
     return tuple(trials)
 
 
 __all__ = [
     "PropertyDict",
+    "TaskControl",
     "OptoTagging",
     "AudRFMapping",
     "VisRFMapping",
     "DynamicRouting1",
+    "get_trials",
 ]
