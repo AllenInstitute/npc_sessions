2cc790412e2977a52fd0015adac3743e893b6fbe
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-13 02:09:29 -0700

    Make all sessions available

--- src/npc_sessions/__init__.py
+++ src/npc_sessions/__init__.py
@@ -6,8 +6,9 @@ from npc_sessions.sessions import *
 from npc_sessions.trials import *
 from npc_sessions.utils import *
 
-Session = DynamicRoutingSession  # temp alias for backwards compatibility
-
 _ = dotenv.load_dotenv(
     dotenv.find_dotenv(usecwd=True)
 )  # take environment variables from .env
+
+Session = DynamicRoutingSession  # temp alias for backwards compatibility
+sessions = [DynamicRoutingSession(info.session) for info in tracked]
