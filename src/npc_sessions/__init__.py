e334d97805be0c9145f19bff7cd3c6cace3819be
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-14 23:33:40 -0700

    Restrict sessions list to those without issues and use config dict

--- src/npc_sessions/__init__.py
+++ src/npc_sessions/__init__.py
@@ -11,5 +11,20 @@ _ = dotenv.load_dotenv(
 )  # take environment variables from .env
 
 Session = DynamicRoutingSession  # temp alias for backwards compatibility
-sessions = [DynamicRoutingSession(info.session) for info in sorted(tracked, key=lambda x: x.date) if info.is_uploaded]
-"""Uploaded sessions, tracked in npc_lims via `tracked_sessions.yaml`"""
+
+session_kwargs = {
+    '670248_2023-08-02': dict(suppress_errors=True),
+    '628801_2022-09-20': dict(is_video=False),
+}
+issues = dict.fromkeys(('660023_2023-08-08', '666986_2023-08-14', '644867_2023-02-21', '628801_2022-09-20'))
+sessions = [
+    DynamicRoutingSession(info.session, **session_kwargs.get(info.session, {})) 
+    for info in sorted(tracked, key=lambda x: x.date)
+    if (
+        info.is_uploaded 
+        and info.session not in issues
+    )
+]
+"""Uploaded sessions, tracked in npc_lims via `tracked_sessions.yaml`. Sessions
+with known issues are excluded. Session-specific config from `session_kwargs`
+is applied."""
