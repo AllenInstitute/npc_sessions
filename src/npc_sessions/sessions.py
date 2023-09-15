9ddbc7a3095169bdea9022f191dc9b6e2304599c
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-15 13:20:44 -0700

    Add __eq__ for checking membership

--- src/npc_sessions/sessions.py
+++ src/npc_sessions/sessions.py
@@ -107,6 +107,12 @@ class DynamicRoutingSession:
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.id!r})"
 
+
+    def __eq__(self, other: Any) -> bool:
+        if other_id := getattr(other, 'id', None):
+            return self.id == other_id
+        return self.id == str(other)
+    
     @property
     def nwb(self) -> pynwb.NWBFile:
         # if self._nwb_hdf5_path:
