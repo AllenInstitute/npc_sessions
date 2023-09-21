830ff1eb58fa358a494e24405c309d9ff7fa7706
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-21 16:39:23 -0700

    Fix types

--- src/npc_sessions/plots/plot_utils.py
+++ src/npc_sessions/plots/plot_utils.py
@@ -1,19 +1,23 @@
+from __future__ import annotations
+
+from typing import Callable, Any
+
 import rich
 
 bad_tag = '[bold red]'
 good_tag = '[bold green]'
 cautious_tag = '[bold yellow]'
 
-def bad_string(base: str):
+def bad_string(base: str) -> str:
     return bad_tag + base + bad_tag
 
-def good_string(base: str):
+def good_string(base: str) -> str:
     return good_tag + base + good_tag
 
-def cautious_string(base: str):
+def cautious_string(base: str) -> str:
     return cautious_tag + base + cautious_tag
 
-def determine_string_valence(value, good_criterion, bad_criterion):
+def determine_string_valence(value: Any, good_criterion: bool, bad_criterion: bool) -> Callable[[str], str]:
     if good_criterion:
         return good_string
 
