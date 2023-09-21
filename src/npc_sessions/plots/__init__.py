2aecdccaba608a6a624584dbd7dccd9f986480d7
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-21 15:57:55 -0700

    Add video plots

--- src/npc_sessions/plots/__init__.py
+++ src/npc_sessions/plots/__init__.py
@@ -3,3 +3,4 @@ from npc_sessions.plots.behavior import *
 from npc_sessions.plots.audio import *
 from npc_sessions.plots.spikes import *
 from npc_sessions.plots.sync import *
+from npc_sessions.plots.video import *
