0bbae780b7eba869a557c8b3287e04100f1e1b53
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-09-28 21:06:08 -0700

    Add spikeinterface data class

--- src/npc_sessions/utils/__init__.py
+++ src/npc_sessions/utils/__init__.py
@@ -6,6 +6,7 @@ from npc_sessions.utils.mvr import *
 from npc_sessions.utils.openephys import *
 from npc_sessions.utils.running import *
 from npc_sessions.utils.settings_xml import *
+from npc_sessions.utils.spikeinterface import *
 from npc_sessions.utils.stim import *
 from npc_sessions.utils.sync import *
 from npc_sessions.utils.units import *
