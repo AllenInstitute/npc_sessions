952e3c168c89db4296540ce1c17055bdd7500973
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-08 11:28:09 -0700

    Add epoch color bars function

--- src/npc_sessions/plots/plot_utils.py
+++ src/npc_sessions/plots/plot_utils.py
@@ -1,11 +1,48 @@
 from __future__ import annotations
 
-from typing import Any, Callable
+from typing import Any, Callable, TYPE_CHECKING 
 
+if TYPE_CHECKING:
+    import matplotlib.pyplot as plt
+    import matplotlib.axes
+    import pandas as pd
+    
 bad_tag = "[bold red]"
 good_tag = "[bold green]"
 cautious_tag = "[bold yellow]"
 
+
+EPOCH_COLOR_MAP = {
+    "RFMapping": "black",
+    "DynamicRouting1": "blue",
+    "OptoTagging": "cyan",
+    "Spontaneous": "red",
+    "SpontaneousRewards": "magenta",
+}
+
+def add_epoch_color_bars(
+    ax: matplotlib.axes.Axes, epochs: pd.DataFrame, 
+    epoch_color_map: dict[str, Any] | None = None,
+    **text_kwargs,
+) -> None:
+    if epoch_color_map is None:
+        epoch_color_map = EPOCH_COLOR_MAP
+    text_kwargs = dict(
+        y=0,
+        ha="center",
+        rotation=0,
+        fontsize=6,
+        zorder=1,
+    ) | text_kwargs
+    for _, epoch in epochs.iterrows():
+        epoch_name = next((k for k in epoch.tags if k in epoch_color_map), "")
+        color = epoch_color_map[epoch_name] if epoch_name else "black"
+        ax.axvspan(epoch.start_time, epoch.stop_time, alpha=0.1, color=color, zorder=0)
+        ax.text(
+            x=(epoch.stop_time + epoch.start_time) / 2,
+            s=epoch_name,
+            **text_kwargs,
+        )
 
 def bad_string(base: str) -> str:
     return bad_tag + base + bad_tag
