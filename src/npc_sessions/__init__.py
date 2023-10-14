9f1018f750e813cdd2e74e9e49c4fcf7ec652af2
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-13 19:45:10 -0700

    Rm nwbwidgets - scikit-image dep causing issues on rig with py3.11

--- src/npc_sessions/__init__.py
+++ src/npc_sessions/__init__.py
@@ -5,7 +5,7 @@ from npc_sessions.plots import *
 from npc_sessions.sessions import *
 from npc_sessions.trials import *
 from npc_sessions.utils import *
-from npc_sessions.widgets import PSTHWidget, session_widget
+from npc_sessions.widgets import session_widget
 
 _ = dotenv.load_dotenv(
     dotenv.find_dotenv(usecwd=True)
@@ -13,3 +13,4 @@ _ = dotenv.load_dotenv(
 
 Session = DynamicRoutingSession
 """Temp alias for backwards compatibility"""
+    
