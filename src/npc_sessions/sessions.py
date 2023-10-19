7d8e5adbde8a3409c3487488c9c375576cee7b59
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-10-19 16:50:16 -0700

    Update video description

--- src/npc_sessions/sessions.py
+++ src/npc_sessions/sessions.py
@@ -1943,7 +1943,7 @@ class DynamicRoutingSession:
             ndx_events.Events(
                 timestamps=timestamps,
                 name=f"{nwb_names[utils.extract_camera_name(path.stem)]}_camera",
-                description=f"start of video frame exposures for {path.stem}",
+                description=f"start time of each frame exposure for {path.stem}",
             )
             for path, timestamps in path_to_timestamps.items()
         )
