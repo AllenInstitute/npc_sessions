ac609e79e3ff4f88e0e1beb4275fc69e2b84c8d6
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-11-03 14:05:09 -0700

    Revert "Fix types"
    This reverts commit 3b43f5cdf580299a8cb225887e3f0e6860baa8e7.

--- write_test.py
+++
@@ -1,9 +0,0 @@
-import npc_lims
-import upath
-
-import npc_sessions
-import npc_sessions.utils as utils
-
-session = next(npc_sessions.get_sessions(is_ephys=False, is_sync=False))
-nwb = session.nwb
-utils.write_nwb(npc_lims.NWB_REPO / f'{session.id}.nwb.zarr', nwb)
