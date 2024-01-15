5af9943dab6177e1f69c649b7c2017d75a5643c7
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-01-14 20:52:18 -0800

    Fix multiprocessing in cache actions #75

--- src/npc_sessions/scripts/write_sessions_to_cache.py
+++ src/npc_sessions/scripts/write_sessions_to_cache.py
@@ -4,6 +4,7 @@ import concurrent.futures
 import datetime
 import logging
 import logging.config
+import multiprocessing
 import time
 from typing import Literal
 
@@ -46,7 +47,7 @@ def write_sessions_to_cache(
         parallel = False
     if parallel:
         future_to_session = {}
-        pool = concurrent.futures.ProcessPoolExecutor(utils.get_max_workers())
+        pool = concurrent.futures.ProcessPoolExecutor(utils.get_max_workers(), mp_context=multiprocessing.get_context('spawn'))
         for info in tqdm.tqdm(session_infos, desc="Submitting jobs"):
             future_to_session[
                 pool.submit(
