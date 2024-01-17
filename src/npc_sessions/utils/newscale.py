6378b879c6cb1991aa74f0e79a4547c7f52e3a6f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-01-16 20:47:04 -0800

    Fix NewScale z- coords

--- src/npc_sessions/utils/newscale.py
+++ src/npc_sessions/utils/newscale.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
+import collections
 
 import datetime
 import logging
+from typing import Iterable
 
 import npc_session
+import numpy as np
 import pandas as pd
 import polars as pl
 
@@ -45,6 +48,9 @@ SERIAL_NUM_TO_PROBE_LETTER = (
         "SN40902": "F",
     }  # NP.3
 )
+SHORT_TRAVEL_SERIAL_NUMBERS = {"SN32148", "SN32142", "SN32144", "SN32149", "SN32135", "SN24273"}
+SHORT_TRAVEL_RANGE = 6_000
+LONG_TRAVEL_RANGE = 15_000
 NEWSCALE_LOG_COLUMNS = (
     "last_movement",
     "device",
@@ -82,28 +88,29 @@ def get_newscale_data_lazy(path: utils.PathLike) -> pl.LazyFrame:
 
 def get_newscale_coordinates(
     newscale_log_path: utils.PathLike,
-    time: str | datetime.datetime | npc_session.DatetimeRecord | None = None,
+    recording_start_time: str | datetime.datetime | npc_session.DatetimeRecord | None = None,
 ) -> pd.DataFrame:
     """Returns the coordinates of each probe at the given time, by scanning for the most-recent prior movement on each motor.
 
-    - looks up the timestamp of movement preceding `time`
-    - if not provided, attempt to parse experiment start time from `newscale_log_path`:
+    - looks up the timestamp of movement preceding `recording_start_time`
+    - if not provided, attempt to parse experiment (sync) start time from `newscale_log_path`:
       assumes manipulators were not moved after the start time
 
     >>> df = get_newscale_coordinates('s3://aind-ephys-data/ecephys_686740_2023-10-23_14-11-05/behavior_videos/log.csv', '2023-10-23 14-11-05')
     >>> list(df['x'])
     [6278.0, 6943.5, 7451.0, 4709.0, 4657.0, 5570.0]
+    >>> list(df['z'])
     """
-    if time is None:
+    if recording_start_time is None:
         p = utils.from_pathlike(newscale_log_path)
         try:
             start = npc_session.DatetimeRecord(p.as_posix())
         except ValueError as exc:
             raise ValueError(
-                f"`time` must be provided to indicate start of ephys recording: no time could be parsed from {p.as_posix()}"
+                f"`recording_start_time` must be provided to indicate start of ephys recording: no time could be parsed from {p.as_posix()}"
             ) from exc
     else:
-        start = npc_session.DatetimeRecord(time)
+        start = npc_session.DatetimeRecord(recording_start_time)
 
     movement = pl.col(NEWSCALE_LOG_COLUMNS[0])
     serial_number = pl.col(NEWSCALE_LOG_COLUMNS[1])
@@ -112,7 +119,16 @@ def get_newscale_coordinates(
         df = get_newscale_data_lazy(newscale_log_path)  # type: ignore [assignment]
     except (pl.ComputeError, OSError):
         df = get_newscale_data(newscale_log_path)
-
+    
+    z_df = df.select(['z'])
+    if isinstance(df, pl.LazyFrame):
+        z_df = z_df.collect()
+    z_values = z_df['z']
+    z_inverted: bool = is_z_inverted(z_values)
+    
+    if isinstance(df, pl.LazyFrame):
+        df = df.collect()
+    
     df = (
         df.filter(movement < start.dt)
         .group_by(serial_number)
@@ -123,7 +139,7 @@ def get_newscale_coordinates(
     )
     if isinstance(df, pl.LazyFrame):
         df = df.collect()
-
+    
     # serial numbers have an extra leading space
     manipulators = df.get_column(NEWSCALE_LOG_COLUMNS[1]).map_elements(
         lambda _: _.strip()
@@ -132,8 +148,49 @@ def get_newscale_coordinates(
     probes = manipulators.map_dict(
         {k: f"probe{v}" for k, v in SERIAL_NUM_TO_PROBE_LETTER.items()}
     ).alias("electrode_group")
+    
+    # correct z values
+    z = df['z']
+    for idx, device in enumerate(df['device']):
+        if z_inverted:
+            z[idx] = get_z_travel(device) - z[idx]
+    df.replace("z", z)
+    
     return df.insert_at_idx(0, probes).sort(pl.col("electrode_group")).to_pandas()
 
+
+def get_z_travel(serial_number: str) -> int:
+    """
+    >>> get_z_travel('SN32144')
+    6000
+    >>> get_z_travel('SN40911')
+    15000
+    """
+    if serial_number not in SERIAL_NUM_TO_PROBE_LETTER:
+        raise ValueError(f"{serial_number=} is not a known serial number: need to update {__file__}")
+    if serial_number in SHORT_TRAVEL_SERIAL_NUMBERS:
+        return SHORT_TRAVEL_RANGE
+    return LONG_TRAVEL_RANGE
+    
+def is_z_inverted(z_values: Iterable[float]) -> bool:
+    """
+    The limits of the z-axis are [0-6000] for NP.0 and [0-15000] for NP.1-3. The
+    NewScale software sometimes (but not consistently) inverts the z-axis, so
+    retracted probes have a z-coordinate of 6000 or 15000 not 0. This function checks
+    the values in the z-column and tries to determine if the z-axis is inverted.
+    
+    Assumptions:
+    - the manipulators spend more time completely retracted than completely extended
+
+    >>> is_z_inverted([0, 3000, 3000, 0])
+    False
+    >>> is_z_inverted([15000, 3000, 3000, 15000])
+    True
+    """
+    c = collections.Counter(z_values)
+    is_long_travel = bool(c[LONG_TRAVEL_RANGE])
+    travel_range = LONG_TRAVEL_RANGE if is_long_travel else SHORT_TRAVEL_RANGE
+    return c[0] < c[travel_range]
 
 if __name__ == "__main__":
     import doctest
