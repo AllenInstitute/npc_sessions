03f85d140a3f61e42db9c1bf97ab7ffc23263299
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-02-21 10:18:26 -0800

    Move running to npc_stim

--- src/npc_sessions/utils/__init__.py
+++ src/npc_sessions/utils/__init__.py
@@ -5,5 +5,4 @@ from npc_sessions.utils.electrodes import *
 from npc_sessions.utils.intervals import *
 from npc_sessions.utils.misc import *
 from npc_sessions.utils.newscale import *
-from npc_sessions.utils.running import *
 from npc_sessions.utils.videos import *
