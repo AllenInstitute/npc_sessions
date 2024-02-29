6bd4f54bfef4dc719a2ae8278cd0a5ce19c74a21
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-02-28 19:52:26 -0800

    Temporarily remove spike_times from cache

--- src/npc_sessions/utils/cache.py
+++ src/npc_sessions/utils/cache.py
@@ -149,6 +149,8 @@ def write_all_components_to_cache(
     """
     logger.info(f"Writing all components to cache for {session.id}")
     for component_name in typing.get_args(npc_lims.NWBComponentStr):
+        if component_name == "spike_times":
+            continue
         # skip before we potentially do a lot of processing to get component
         if (
             skip_existing
