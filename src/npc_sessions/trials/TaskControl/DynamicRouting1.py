b738f6199eccc34ff9b7ecd89ba0c3255e75dc4e
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-04-16 23:26:50 -0700

    Fix test

--- src/npc_sessions/trials/TaskControl/DynamicRouting1.py
+++ src/npc_sessions/trials/TaskControl/DynamicRouting1.py
@@ -43,7 +43,7 @@ class DynamicRouting1(TaskControl):
     ...    nwb_file.add_trial(**trial)
 
     >>> trials = DynamicRouting1('s3://aind-ephys-data/ecephys_670248_2023-08-03_12-04-15/behavior/DynamicRouting1_670248_20230803_123154.hdf5')
-    >>> assert not trials._df.is_empty()
+    >>> assert not trials.to_dataframe().empty
     """
 
     _num_opto_devices: int | None = None
