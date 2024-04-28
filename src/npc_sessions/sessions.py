0a914da72812cd09201a3e5c1799de3e5029068f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-04-28 13:14:01 -0700

    Split up `nwb` and `nwb_from_cache`

--- src/npc_sessions/sessions.py
+++ src/npc_sessions/sessions.py
@@ -293,12 +293,16 @@ class DynamicRoutingSession:
         return getattr(self, "_suppress_errors", False)
 
     @property
-    def nwb(self) -> pynwb.NWBFile:
+    def nwb_from_cache(self) -> pynwb.NWBFile | None:
         if (path := npc_lims.get_nwb_path(self.id)).exists():
             if path.suffix == ".zarr":
                 return hdmf_zarr.NWBZarrIO(path=path.as_posix(), mode="r").read()
             else:
                 return pynwb.NWBHDF5IO(path=path.as_posix(), mode="r").read()
+        return None
+    
+    @property
+    def nwb(self) -> pynwb.NWBFile:
         return pynwb.NWBFile(
             session_id=self.session_id,
             session_description=self.session_description,
