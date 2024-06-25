f8140cf659a03eebe17a67bafba1717ca6d743db
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-06-25 13:31:30 -0700

    Update test for aind session json to include writing to disk

--- tests/test_aind_metadata.py
+++ tests/test_aind_metadata.py
@@ -1,8 +1,14 @@
+import tempfile
+
 import npc_sessions
 
 def test_aind_session_metadata():
-    s = npc_sessions.DynamicRoutingSession('DRpilot_676909_20231213-08-03')
-    _ = s._aind_session_metadata
+    ephys_session = 'DRpilot_676909_20231213-08-03'
+    behavior_session = '715706_20240529'
+    for session in [behavior_session, ephys_session]:
+        s = npc_sessions.DynamicRoutingSession(session)
+        m = s._aind_session_metadata
+        m.write_standard_file(tempfile.mkdtemp())
     
 if __name__ == "__main__":
     import pytest
