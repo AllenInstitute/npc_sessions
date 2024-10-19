4d12b62b2cc4b3442bf9c5dbd1f0daf847e77dee
bjhardcastle <bjhardcastle@users.noreply.github.com>
2024-10-19 08:18:51 +0000

    Bump [skip actions]

--- src/npc_sessions/sessions.py
+++ src/npc_sessions/sessions.py
@@ -3747,10 +3747,8 @@ class DynamicRoutingSurfaceRecording(DynamicRoutingSession):
 
     @npc_io.cached_property
     def sorted_data_paths(self) -> tuple[upath.UPath, ...]:
-        return npc_lims.get_sorted_data_paths_from_s3(
-            self.id.with_idx(1)
-        )
-        
+        return npc_lims.get_sorted_data_paths_from_s3(self.id.with_idx(1))
+
     @npc_io.cached_property
     def ephys_timing_data(self) -> tuple[npc_ephys.EphysTimingInfo, ...]:
         """Sync data not available, so timing info not accurate"""
