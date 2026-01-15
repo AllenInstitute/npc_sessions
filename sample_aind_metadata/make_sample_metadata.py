36ad85db78a0afb0ee690d1150ccb9df12afd126
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-01-14 18:37:09 -0800

    Fix output path

--- sample_aind_metadata/make_sample_metadata.py
+++ sample_aind_metadata/make_sample_metadata.py
@@ -2,8 +2,8 @@ import npc_sessions
 import npc_sessions.aind_data_schema
 
 session_id = '676909_2023-12-13' # DR
-suffix = f"_{session_id}"
-output_directory = '.sample_aind_metadata'
+suffix = f"_{session_id}.json"
+output_directory = 'sample_aind_metadata'
 
 session = npc_sessions.DynamicRoutingSession(session_id)
 
